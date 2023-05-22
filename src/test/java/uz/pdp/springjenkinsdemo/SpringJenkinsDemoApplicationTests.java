334285d240fb20284a9830085c6cfb11db53efba
@javag17 <sunnatjavadeveloper@gmail.com>
2023-05-22 21:38:22 +0500

    commit hello

---
+++ src/test/java/uz/pdp/springjenkinsdemo/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springjenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
